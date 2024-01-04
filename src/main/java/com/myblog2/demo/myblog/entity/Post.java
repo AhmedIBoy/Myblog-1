1d4f58197810630d7306dda48341ab12a5ce0671
ahmednandoliya <Iboy85000@gmail.com>
2024-01-04 11:58:27 +0530

    127:Create Save Post Feature Database Table Added

---
+++ src/main/java/com/myblog2/demo/myblog/entity/Post.java
@@ -0,0 +1,23 @@
+package com.myblog2.demo.myblog.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "posts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String tittle;
+
+    private String description;
+
+    private String content;
+}
